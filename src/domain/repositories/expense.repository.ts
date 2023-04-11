0efeb6f7c6e462e11f5918655ab1cd7c8fb05738
dieegomr <dmendes.rocha@gmail.com>
2023-04-11 10:29:26 -0300

    feat: add findById to repository

--- src/domain/repositories/expense.repository.ts
+++ src/domain/repositories/expense.repository.ts
@@ -2,4 +2,5 @@ import { Expense } from '../entities/expense/expense.entity';
 
 export interface ExpenseRepositoryInterface {
   insert(expense: Expense): Promise<void>;
+  findById(id: string): Promise<Expense | null>;
 }
--- src/infra/expense-in-memory.repository.ts
+++ src/infra/expense-in-memory.repository.ts
@@ -7,4 +7,12 @@ export class ExpenseInMemoryRepository implements ExpenseRepositoryInterface {
   async insert(expense: Expense): Promise<void> {
     this.items.push(expense);
   }
+
+  async findById(id: string): Promise<Expense | null> {
+    const expense = this.items.find((expense) => expense.id === id);
+
+    if (!expense) return null;
+
+    return expense;
+  }
 }
