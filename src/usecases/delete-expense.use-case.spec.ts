14ca69e72ef7772c5781e153259a6d9ad02a666f
dieegomr <dmendes.rocha@gmail.com>
2023-04-11 15:07:56 -0300

    feat: add delete expense usecase

---
+++ src/usecases/delete-expense.use-case.spec.ts
@@ -0,0 +1,34 @@
+import { Expense } from '../domain/entities/expense/expense.entity';
+import { ExpenseInMemoryRepository } from '../infra/expense-in-memory.repository';
+import { CreateExpenseUseCase } from './create-expense.use-case';
+import { DeleteExpenseByIdUseCase } from './delete-expense.use-case';
+import { ExpenseNotFoundError } from './errors/get-expense-by-id.error';
+
+describe('DeleteExpenseUseCase Tests', function () {
+  it('should delete a expense', async function () {
+    const repository = new ExpenseInMemoryRepository();
+    const createUseCase = new CreateExpenseUseCase(repository);
+    const deleteExpenseById = new DeleteExpenseByIdUseCase(repository);
+    const expenseOrError = await createUseCase.execute({
+      description: 'expense description',
+      date: new Date(2022, 3, 10),
+      amount: 120,
+      user: 'userID',
+    });
+
+    const expense = expenseOrError.value as Expense;
+
+    const deletedOrError = await deleteExpenseById.execute(expense.id);
+
+    expect(repository.items).toHaveLength(0);
+  });
+
+  it('should return an error if could not find a expense', async function () {
+    const repository = new ExpenseInMemoryRepository();
+    const deleteExpenseById = new DeleteExpenseByIdUseCase(repository);
+
+    const deletedOrError = await deleteExpenseById.execute('wrongId');
+
+    expect(deletedOrError.value).toBeInstanceOf(ExpenseNotFoundError);
+  });
+});
---
+++ src/usecases/delete-expense.use-case.ts
@@ -0,0 +1,27 @@
+import { Expense } from '../domain/entities/expense/expense.entity';
+import { ExpenseRepositoryInterface } from '../domain/repositories/expense.repository';
+import { Either, left, right } from '../shared/either';
+import { ExpenseNotFoundError } from './errors/get-expense-by-id.error';
+
+export class DeleteExpenseByIdUseCase {
+  constructor(private readonly expenseRepo: ExpenseRepositoryInterface) {}
+
+  async execute(
+    id: string
+  ): Promise<Either<ExpenseNotFoundError, DeleteExpenseOutput>> {
+    const expense = await this.expenseRepo.findById(id);
+    if (!expense) return left(new ExpenseNotFoundError());
+
+    await this.expenseRepo.deleteById(expense.id);
+
+    return right(expense.toJSON());
+  }
+}
+
+type DeleteExpenseOutput = {
+  id: string;
+  description: string;
+  date: Date;
+  user: string;
+  amount: number;
+};
