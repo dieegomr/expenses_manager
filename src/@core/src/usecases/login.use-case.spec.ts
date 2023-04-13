c0f4fc3eaf6497a30212a2ae722ff4f56a4ef5ce
dieegomr <dmendes.rocha@gmail.com>
2023-04-13 07:49:47 -0300

    feat: ensure Login UseCase return MissingParamError if no password is provided

--- src/@core/src/usecases/login.use-case.spec.ts
+++ src/@core/src/usecases/login.use-case.spec.ts
@@ -2,9 +2,15 @@ import { MissingParamError } from './errors/missing-param.error';
 import { LoginUseCase } from './login.use-case';
 
 describe('Login UseCase', function () {
-  test('should return null if no email is provided', async function () {
+  test('should return MissingParamError if no email is provided', async function () {
     const login = new LoginUseCase();
     const accessToken = await login.execute();
+    expect(accessToken).toBeInstanceOf(MissingParamError);
+  });
+
+  test('should return MissingParamError if no password is provided', async function () {
+    const login = new LoginUseCase();
+    const accessToken = await login.execute('any_email@gmail.com');
     expect(accessToken).toBeInstanceOf(MissingParamError);
   });
 });
--- src/@core/src/usecases/login.use-case.ts
+++ src/@core/src/usecases/login.use-case.ts
@@ -1,7 +1,8 @@
 import { MissingParamError } from './errors/missing-param.error';
 
 export class LoginUseCase {
-  async execute(email) {
-    if (!email) return new MissingParamError();
+  async execute(email, password) {
+    if (!email) return new MissingParamError('email');
+    if (!password) return new MissingParamError('password');
   }
 }
