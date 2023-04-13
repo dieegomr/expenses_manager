114f1ffbfaf2d702cc709f9ab83ea0ee7036a27f
dieegomr <dmendes.rocha@gmail.com>
2023-04-13 10:17:03 -0300

    feat: add toJSON method

--- src/@core/domain/entities/user/user.entity.ts
+++ src/@core/domain/entities/user/user.entity.ts
@@ -1,10 +1,10 @@
-import { Either, left } from '../../../shared/either';
+import { Either, left, right } from '../../../shared/either';
 import { InvalidEmailError } from '../email/emai.errors';
 import { Email } from '../email/email.entity';
 
 export type UserProps = {
   name: string;
-  email: Email;
+  email: string;
   password: string;
 };
 
@@ -17,13 +17,22 @@ export class User {
     this._id = id;
   }
 
-  static create(input: UserProps): Either<InvalidEmailError, User> {
-    const emailOrError = Email.create(input.email.value);
-    if (emailOrError.isLeft())
-      return left(new InvalidEmailError(input.email.value));
+  static create(input: UserProps, id: string): Either<InvalidEmailError, User> {
+    const emailOrError = Email.create(input.email);
+    if (emailOrError.isLeft()) return left(new InvalidEmailError(input.email));
+
+    return right(new User(input, id));
   }
 
   public get id() {
     return this._id;
+  }
+
+  public toJSON() {
+    return {
+      id: this.id,
+      name: this.props.name,
+      email: this.props.email,
+    };
   }
 }
