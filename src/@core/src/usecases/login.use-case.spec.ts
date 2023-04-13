c7fd1aabd0bfdf7fe354b38e6ae50a925c0f76c0
dieegomr <dmendes.rocha@gmail.com>
2023-04-13 08:36:25 -0300

    feat: ensure login usecase call user repository findByemail method with correct email

--- src/@core/src/usecases/login.use-case.spec.ts
+++ src/@core/src/usecases/login.use-case.spec.ts
@@ -1,3 +1,4 @@
+import { UserInMemoryRepository } from '../infra/user-in-memory.repository';
 import { MissingParamError } from './errors/missing-param.error';
 import { LoginUseCase } from './login.use-case';
 
@@ -12,5 +13,16 @@ describe('Login UseCase', function () {
     const login = new LoginUseCase();
     const accessToken = await login.execute('any_email@gmail.com');
     expect(accessToken).toBeInstanceOf(MissingParamError);
+  });
+
+  test('should call load user by email repository when an email is provided', async function () {
+    const loadUserByEmailSpy = jest.spyOn(
+      UserInMemoryRepository.prototype,
+      'findByEmail',
+    );
+    const userRepository = new UserInMemoryRepository();
+    const login = new LoginUseCase(userRepository);
+    await login.execute('any_email@gmail.com', 'any_password');
+    expect(loadUserByEmailSpy).toHaveBeenCalledWith('any_email@gmail.com');
   });
 });
--- src/@core/src/usecases/login.use-case.ts
+++ src/@core/src/usecases/login.use-case.ts
@@ -1,8 +1,11 @@
+import { UserRepositoryInterface } from '../domain/repositories/user.repository';
 import { MissingParamError } from './errors/missing-param.error';
 
 export class LoginUseCase {
+  constructor(private readonly userRepository: UserRepositoryInterface) {}
   async execute(email, password) {
     if (!email) return new MissingParamError('email');
     if (!password) return new MissingParamError('password');
+    await this.userRepository.findByEmail(email);
   }
 }
