f4f279122da621b7d3711cfb13f3d91fde789f5c
dieegomr <dmendes.rocha@gmail.com>
2023-04-13 09:44:45 -0300

    refactor: add email validation to entity

--- src/@core/src/domain/entities/user/user.entity.spec.ts
+++ src/@core/src/domain/entities/user/user.entity.spec.ts
@@ -1,16 +1,22 @@
+import { Email } from '../email/email.entity';
 import { User } from './user.entity';
 
 describe('User Tests', () => {
   it('should create a user with id', function () {
+    const emailOrError = Email.create('diego@gmail.com');
+    const email = emailOrError.value as Email;
+
     const props = {
       name: 'some_user_name',
       password: 'some_user_password',
+      email: email,
     };
     const user = new User(props, 'some_user_id');
 
     expect(user.id).toBe('some_user_id');
     expect(user.props.name).toBe('some_user_name');
     expect(user.props.password).toBe('some_user_password');
+    expect(user.props.email.value).toBe('diego@gmail.com');
   });
 
   it('should create a user without id', function () {
--- src/@core/src/domain/entities/user/user.entity.ts
+++ src/@core/src/domain/entities/user/user.entity.ts
@@ -1,6 +1,10 @@
+import { Either, left } from 'src/@core/src/shared/either';
+import { InvalidEmailError } from '../email/emai.errors';
+import { Email } from '../email/email.entity';
+
 export type UserProps = {
   name: string;
-  email: string;
+  email: Email;
   password: string;
 };
 
@@ -11,6 +15,12 @@ export class User {
   constructor(props: UserProps, id?: string) {
     this.props = props;
     this._id = id;
+  }
+
+  static create(input: UserProps): Either<InvalidEmailError, User> {
+    const emailOrError = Email.create(input.email.value);
+    if (emailOrError.isLeft())
+      return left(new InvalidEmailError(input.email.value));
   }
 
   public get id() {
