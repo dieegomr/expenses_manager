606d85bd2061caedeb393f892693a1cbacb9ae25
dieegomr <dmendes.rocha@gmail.com>
2023-04-13 07:55:06 -0300

    feat: add user repository interface

---
+++ src/@core/src/domain/repositories/user.repository.ts
@@ -0,0 +1,6 @@
+import { User } from '../entities/user/user.entity';
+
+export interface UserRepositoryInterface {
+  save(user: User): Promise<void>;
+  findByEmail(email: string): Promise<User | null>;
+}
