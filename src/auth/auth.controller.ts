53c30527c16d28ae4c85c08ea907c48b7e994473
dieegomr <dmendes.rocha@gmail.com>
2023-04-14 10:10:24 -0300

    feat: add auth controller

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,25 @@
+import {
+  Body,
+  Controller,
+  HttpException,
+  HttpStatus,
+  Post,
+} from '@nestjs/common';
+import { LoginUseCase } from 'src/@core/usecases/login.use-case';
+import { LoginDto } from './dto/login.dto';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private readonly loginUseCase: LoginUseCase) {}
+  @Post()
+  async login(@Body() loginDto: LoginDto) {
+    const { email, password } = loginDto;
+    const outputOrError = await this.loginUseCase.execute(email, password);
+    if (outputOrError.isLeft())
+      throw new HttpException(
+        outputOrError.value.message,
+        HttpStatus.BAD_REQUEST,
+      );
+    return outputOrError.value;
+  }
+}
