6cb922decfeae78cb668fde7145b3372f0f6263d
dieegomr <dmendes.rocha@gmail.com>
2023-04-14 17:14:23 -0300

    refactor: update test

--- src/@core/usecases/login.use-case.spec.ts
+++ src/@core/usecases/login.use-case.spec.ts
@@ -4,24 +4,33 @@ import { UserInMemoryRepository } from '../infra/user-in-memory.repository';
 import { CreateUserOutput, CreateUserUseCase } from './create-user.use-case';
 import { LoginError } from './errors/login.error';
 import { MissingParamError } from './errors/missing-param.error';
+import { GetUserByEmailUseCase } from './get-user-by-email.use-case';
 import { LoginOutput, LoginUseCase } from './login.use-case';
 
 describe('Login UseCase', function () {
+  const makeLoging = () => {
+    const userRepository = new UserInMemoryRepository();
+    const bcrypt = new BcryptPasswordHashing();
+    const jwt = new JwtTokenGenerator();
+    const getUserByEmail = new GetUserByEmailUseCase(userRepository);
+    const createUserUseCase = new CreateUserUseCase(userRepository, bcrypt);
+    const login = new LoginUseCase(getUserByEmail, bcrypt, jwt);
+    return { login, bcrypt, jwt, getUserByEmail, createUserUseCase };
+  };
   test('should return MissingParamError if no email is provided', async function () {
-    const login = new LoginUseCase();
+    const { login } = makeLoging();
     const accessToken = await login.execute();
     expect(accessToken.value).toBeInstanceOf(MissingParamError);
   });
 
   test('should return MissingParamError if no password is provided', async function () {
-    const login = new LoginUseCase();
+    const { login } = makeLoging();
     const accessToken = await login.execute('any_email@gmail.com');
     expect(accessToken.value).toBeInstanceOf(MissingParamError);
   });
 
   test('should return LoginError if wrong email is provided', async function () {
-    const userRepository = new UserInMemoryRepository();
-    const login = new LoginUseCase(userRepository);
+    const { login } = makeLoging();
     const output = await login.execute(
       'invalid_email@gmail.com',
       'any_password',
@@ -30,55 +39,36 @@ describe('Login UseCase', function () {
   });
 
   test('should return LoginError if wrong password is provided', async function () {
-    const userRepository = new UserInMemoryRepository();
-    const createUser = new CreateUserUseCase(userRepository);
-    const passwordHashing = new BcryptPasswordHashing();
-    const tokenGenerator = new JwtTokenGenerator();
+    const { createUserUseCase, login, bcrypt } = makeLoging();
 
-    const hasehdPassword = await passwordHashing.hash('Test@12345');
+    const hasehdPassword = await bcrypt.hash('Test@12345');
 
-    const newUser = await createUser.execute({
+    const newUser = await createUserUseCase.execute({
       name: 'Diego',
       email: 'dmendes.rocha@gmail.com',
       password: hasehdPassword,
     });
 
-    const login = new LoginUseCase(
-      userRepository,
-      passwordHashing,
-      tokenGenerator,
-    );
     const output = await login.execute('dmendes.rocha@gmail.com', 'Test@1234');
     expect(output.value).toBeInstanceOf(LoginError);
   });
 
   test('should return a valid Token if correct password and email are provided', async function () {
-    const userRepository = new UserInMemoryRepository();
-    const createUser = new CreateUserUseCase(userRepository);
-    const passwordHashing = new BcryptPasswordHashing();
-    const tokenGenerator = new JwtTokenGenerator();
+    const { bcrypt, createUserUseCase, login, jwt } = makeLoging();
 
-    const hasehdPassword = await passwordHashing.hash('Test@1234');
-
-    const newUserOrError = await createUser.execute({
+    const newUserOrError = await createUserUseCase.execute({
       name: 'Diego',
       email: 'dmendes.rocha@gmail.com',
-      password: hasehdPassword,
+      password: 'Test@1234',
     });
     const newUser = newUserOrError.value as CreateUserOutput;
-
-    const login = new LoginUseCase(
-      userRepository,
-      passwordHashing,
-      tokenGenerator,
-    );
 
     const loginOutputOrError = await login.execute(
       'dmendes.rocha@gmail.com',
       'Test@1234',
     );
     const output = loginOutputOrError.value as LoginOutput;
-    const { id } = await tokenGenerator.verify(output.token);
+    const { id } = await jwt.verify(output.token);
 
     expect(id).toBe(newUser.id);
   });
