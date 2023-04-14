6f268553227257272f35683aeeb86581eea4cd8f
dieegomr <dmendes.rocha@gmail.com>
2023-04-14 10:11:33 -0300

    chore: delete unnecessay code

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,17 +1,11 @@
 import {
   Controller,
-  Get,
   Post,
   Body,
-  Patch,
-  Param,
-  Delete,
   HttpException,
   HttpStatus,
 } from '@nestjs/common';
-import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
 import { CreateUserUseCase } from 'src/@core/usecases/create-user.use-case';
 
 @Controller('user')
@@ -25,24 +19,4 @@ export class UserController {
       throw new HttpException(output.value.message, HttpStatus.BAD_REQUEST);
     return output.value;
   }
-
-  // @Get()
-  // findAll() {
-  //   return this.userService.findAll();
-  // }
-
-  // @Get(':id')
-  // findOne(@Param('id') id: string) {
-  //   return this.userService.findOne(+id);
-  // }
-
-  // @Patch(':id')
-  // update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
-  //   return this.userService.update(+id, updateUserDto);
-  // }
-
-  // @Delete(':id')
-  // remove(@Param('id') id: string) {
-  //   return this.userService.remove(+id);
-  // }
 }
