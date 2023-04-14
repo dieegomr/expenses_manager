6907d25205c281c40a8daaca8a3cb78cedd7c227
dieegomr <dmendes.rocha@gmail.com>
2023-04-14 17:31:00 -0300

    refactor: update the method to ask for userId

--- src/@core/domain/repositories/expense.repository.ts
+++ src/@core/domain/repositories/expense.repository.ts
@@ -2,7 +2,10 @@ import { Expense } from '../entities/expense/expense.entity';
 
 export interface ExpenseRepositoryInterface {
   insert(expense: Expense): Promise<void>;
-  findById(id: string): Promise<Expense | null>;
+  findByUserIdAndExpenseById(
+    useId: string,
+    expenseId: string,
+  ): Promise<Expense | null>;
   getAllByUser(id: string): Promise<Expense[]>;
   update(expense: Expense): Promise<Expense>;
   deleteById(id: string): Promise<void>;
