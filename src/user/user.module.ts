603f49a2a3ea29f7de320406e11f032299cb862d
dieegomr <dmendes.rocha@gmail.com>
2023-04-14 10:12:24 -0300

    feat: export getUserByEmailUseCase

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -4,6 +4,10 @@ import { UserController } from './user.controller';
 import { UserInMemoryRepository } from 'src/@core/infra/user-in-memory.repository';
 import { CreateUserUseCase } from 'src/@core/usecases/create-user.use-case';
 import { UserRepositoryInterface } from 'src/@core/domain/repositories/user.repository';
+import { PasswordHashing } from 'src/@core/domain/interfaces/password-hashing.interface';
+import { BcryptPasswordHashing } from 'src/@core/infra/bcrypt/bcrypt-password-hashing';
+import { JwtTokenGenerator } from 'src/@core/infra/JWT/jwt-token-generator';
+import { GetUserByEmailUseCase } from 'src/@core/usecases/get-user-by-email.use-case';
 
 @Module({
   controllers: [UserController],
@@ -14,12 +18,35 @@ import { UserRepositoryInterface } from 'src/@core/domain/repositories/user.repo
       useClass: UserInMemoryRepository,
     },
     {
+      provide: BcryptPasswordHashing,
+      useClass: BcryptPasswordHashing,
+    },
+    {
+      provide: GetUserByEmailUseCase,
+      useClass: GetUserByEmailUseCase,
+    },
+    {
+      provide: JwtTokenGenerator,
+      useClass: JwtTokenGenerator,
+    },
+    {
       provide: CreateUserUseCase,
+      useFactory: (
+        userRepo: UserRepositoryInterface,
+        passwordHashing: PasswordHashing,
+      ) => {
+        return new CreateUserUseCase(userRepo, passwordHashing);
+      },
+      inject: [UserInMemoryRepository, BcryptPasswordHashing],
+    },
+    {
+      provide: GetUserByEmailUseCase,
       useFactory: (userRepo: UserRepositoryInterface) => {
-        return new CreateUserUseCase(userRepo);
+        return new GetUserByEmailUseCase(userRepo);
       },
       inject: [UserInMemoryRepository],
     },
   ],
+  exports: [GetUserByEmailUseCase],
 })
 export class UserModule {}
