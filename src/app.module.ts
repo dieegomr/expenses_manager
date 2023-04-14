53ac4e666057c8f5fdc68a9bbad9ce2866d11f73
dieegomr <dmendes.rocha@gmail.com>
2023-04-14 10:12:51 -0300

    feat: add import AuthModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ExpensesModule } from './expenses/expenses.module';
 import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [ExpensesModule, UserModule],
+  imports: [ExpensesModule, UserModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
