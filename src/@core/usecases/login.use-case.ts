19e2c8c6e35876783bbbe2e444356d2c1f7d6b96
dieegomr <dmendes.rocha@gmail.com>
2023-04-14 10:09:33 -0300

    refactor: add getUserByEmail usecase

--- src/@core/usecases/login.use-case.ts
+++ src/@core/usecases/login.use-case.ts
@@ -1,13 +1,13 @@
 import { PasswordHashing } from '../domain/interfaces/password-hashing.interface';
 import { TokenGenerator } from '../domain/interfaces/token-generator.interface';
-import { UserRepositoryInterface } from '../domain/repositories/user.repository';
 import { Either, left, right } from '../shared/either';
 import { LoginError } from './errors/login.error';
 import { MissingParamError } from './errors/missing-param.error';
+import { GetUserByEmailUseCase } from './get-user-by-email.use-case';
 
 export class LoginUseCase {
   constructor(
-    private readonly userRepository: UserRepositoryInterface,
+    private readonly getUserByEmail: GetUserByEmailUseCase,
     private readonly passwordHasher: PasswordHashing,
     private readonly tokenGenerator: TokenGenerator,
   ) {}
@@ -17,22 +17,22 @@ export class LoginUseCase {
   ): Promise<Either<MissingParamError, LoginOutput>> {
     if (!email) return left(new MissingParamError('email'));
     if (!password) return left(new MissingParamError('password'));
-    const user = await this.userRepository.findByEmail(email);
-    if (!user) return left(new LoginError());
+    const user = await this.getUserByEmail.execute(email);
+    if (user.isLeft()) return left(new LoginError());
 
     const isValid = await this.passwordHasher.compare(
       password,
-      user.props.password,
+      user.value.password,
     );
 
     if (!isValid) return left(new LoginError());
 
-    const token = await this.tokenGenerator.sign(user.id);
+    const token = await this.tokenGenerator.sign(user.value.id);
 
     return right({
-      id: user.id,
-      name: user.props.name,
-      email: user.props.email,
+      id: user.value.id,
+      name: user.value.name,
+      email: user.value.email,
       token,
     });
   }
