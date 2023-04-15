81e2109b03659be13bcc35da4700a15114c2c9a2
dieegomr <dmendes.rocha@gmail.com>
2023-04-15 14:46:04 -0300

    feat: send email when create a new expense

--- src/@core/usecases/create-expense.use-case.ts
+++ src/@core/usecases/create-expense.use-case.ts
@@ -8,9 +8,15 @@ import {
   InvalidDescriptionError,
 } from '../domain/entities/expense/expense.errors';
 import { ExpenseRepositoryInterface } from '../domain/repositories/expense.repository';
+import { UserRepositoryInterface } from '../domain/repositories/user.repository';
+import { EmailSender } from '../domain/interfaces/email-sender.interface';
 
 export class CreateExpenseUseCase {
-  constructor(private readonly expenseRepo: ExpenseRepositoryInterface) {}
+  constructor(
+    private readonly expenseRepo: ExpenseRepositoryInterface,
+    private readonly userRepo: UserRepositoryInterface,
+    private readonly emailSender: EmailSender,
+  ) {}
 
   async execute(
     input: CreateExpenseInput,
@@ -23,10 +29,23 @@ export class CreateExpenseUseCase {
   > {
     const uuid = crypto.randomUUID();
 
+    const user = await this.userRepo.findById(userId);
+
     const expense = Expense.create(input, userId, uuid);
     if (expense.isLeft()) return left(expense.value);
 
     await this.expenseRepo.insert(expense.value);
+
+    await this.emailSender.sendEmail({
+      email: user.props.email,
+      subject: 'Despesa Cadastrada',
+      text: `Detalhes da despesa: 
+      id: ${expense.value.id}
+      descrição: ${expense.value.description}
+      data: ${expense.value.date}
+      valor: R$${expense.value.amount} 
+      `,
+    });
 
     return right(expense.value.toJSON());
   }
