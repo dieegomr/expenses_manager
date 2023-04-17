4d23092d13eb147accb08a220bb1e4687259c3ba
dieegomr <dmendes.rocha@gmail.com>
2023-04-17 14:44:12 -0300

    test: ensure a valid email is provided

---
+++ src/@core/domain/entities/email/email.entity.spec.ts
@@ -0,0 +1,25 @@
+import { Email } from './email.entity';
+
+describe('Email Tests', () => {
+  it('should return an error if an invalid email is provided', function () {
+    const emailTest = 'dmende@br';
+
+    const emailOrError = Email.create(emailTest);
+    const error = emailOrError.value as Error;
+
+    console.log(emailOrError.value);
+
+    expect(error.message).toStrictEqual(`The email ${emailTest} is invalid`);
+  });
+
+  it('should return an email if an valid email is provided', function () {
+    const emailTest = 'diego@gmail.br';
+
+    const emailOrError = Email.create(emailTest);
+    const email = emailOrError.value as Email;
+
+    console.log(emailOrError.value);
+
+    expect(email.value).toStrictEqual(emailTest);
+  });
+});
