9717f81c6f16b85b4a067ccf29d6fc486e9b5270
dieegomr <dmendes.rocha@gmail.com>
2023-04-18 10:19:08 -0300

    test: ensure the correct params are passed to delete expense usecase

--- src/@core/usecases/delete-expense.use-case.spec.ts
+++ src/@core/usecases/delete-expense.use-case.spec.ts
@@ -1,18 +1,29 @@
 import { ExpenseInMemoryRepository } from '../infra/expense-in-memory.repository';
+import { UserInMemoryRepository } from '../infra/user-in-memory.repository';
 import { CreateExpenseUseCase } from './create-expense.use-case';
 import { DeleteExpenseByIdUseCase } from './delete-expense.use-case';
 import { ExpenseNotFoundError } from './errors/get-expense-by-id.error';
 import { ExpenseOutput } from './get-expense-by-id.use-case';
+import { GetUserByIdUseCase } from './get-user-by-id.use-case';
+import { FakeEmailSender } from './test-utils/fake-email-sender';
 
 describe('DeleteExpenseUseCase Tests', function () {
   const makeTest = () => {
-    const repository = new ExpenseInMemoryRepository();
-    const createExpenseUseCase = new CreateExpenseUseCase(repository);
-    const deleteExpenseById = new DeleteExpenseByIdUseCase(repository);
-    return { createExpenseUseCase, deleteExpenseById, repository };
+    const expenseRepo = new ExpenseInMemoryRepository();
+    const userRepo = new UserInMemoryRepository();
+    const getUserById = new GetUserByIdUseCase(userRepo);
+    const fakeEmailSender = new FakeEmailSender();
+    const createExpenseUseCase = new CreateExpenseUseCase(
+      expenseRepo,
+      getUserById,
+      fakeEmailSender,
+    );
+    const deleteExpenseById = new DeleteExpenseByIdUseCase(expenseRepo);
+
+    return { createExpenseUseCase, deleteExpenseById, expenseRepo };
   };
   it('should be able to delete an expense if belongs to the current user', async function () {
-    const { createExpenseUseCase, deleteExpenseById, repository } = makeTest();
+    const { createExpenseUseCase, deleteExpenseById, expenseRepo } = makeTest();
     const expenseOrError = await createExpenseUseCase.execute(
       {
         description: 'expense1',
@@ -24,16 +35,13 @@ describe('DeleteExpenseUseCase Tests', function () {
 
     const expense = expenseOrError.value as ExpenseOutput;
 
-    const deletedOrError = await deleteExpenseById.execute(
-      'userId1',
-      expense.id,
-    );
+    await deleteExpenseById.execute('userId1', expense.id);
 
-    expect(repository.items).toHaveLength(0);
+    expect(expenseRepo.items).toHaveLength(0);
   });
 
   it('should return an error if could not find a expense', async function () {
-    const { createExpenseUseCase, deleteExpenseById, repository } = makeTest();
+    const { createExpenseUseCase, deleteExpenseById } = makeTest();
     await createExpenseUseCase.execute(
       {
         description: 'expense1',
@@ -52,7 +60,8 @@ describe('DeleteExpenseUseCase Tests', function () {
   });
 
   it('should return an error if user does not belong to the expense', async function () {
-    const { createExpenseUseCase, deleteExpenseById, repository } = makeTest();
+    const { createExpenseUseCase, deleteExpenseById } = makeTest();
+
     const expenseOrError = await createExpenseUseCase.execute(
       {
         description: 'expense1',
