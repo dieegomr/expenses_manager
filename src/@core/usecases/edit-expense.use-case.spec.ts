9fff44a821d314bc2ef184f4bc2052fc944c976b
dieegomr <dmendes.rocha@gmail.com>
2023-04-18 14:24:32 -0300

    test: pass missing parameter to dependecies

--- src/@core/usecases/edit-expense.use-case.spec.ts
+++ src/@core/usecases/edit-expense.use-case.spec.ts
@@ -1,46 +1,58 @@
 import { InvalidAmountError } from '../domain/entities/expense/expense.errors';
 import { User } from '../domain/entities/user/user.entity';
+import { BcryptPasswordHashing } from '../infra/bcrypt/bcrypt-password-hashing';
 import { ExpenseInMemoryRepository } from '../infra/expense-in-memory.repository';
-import { NodeMailerEmailSender } from '../infra/nodeMailer/nodemailer-email-sender';
 import { UserInMemoryRepository } from '../infra/user-in-memory.repository';
 import { CreateExpenseUseCase } from './create-expense.use-case';
+import { CreateUserOutput, CreateUserUseCase } from './create-user.use-case';
 import { EditExpenseUseCase } from './edit-expense.use-case';
 import { ExpenseOutput } from './get-expense-by-id.use-case';
+import { GetUserByIdUseCase } from './get-user-by-id.use-case';
+import { FakeEmailSender } from './test-utils/fake-email-sender';
 
 describe('EditExpenseUseCase Tests', function () {
-  it('should edit a expense', async function () {
-    const repository = new ExpenseInMemoryRepository();
+  const makeTest = () => {
+    const expenseRepo = new ExpenseInMemoryRepository();
     const userRepo = new UserInMemoryRepository();
-    const emailSender = new NodeMailerEmailSender();
-    const createUseCase = new CreateExpenseUseCase(
-      repository,
-      userRepo,
-      emailSender,
+    const passwordHashing = new BcryptPasswordHashing();
+    const createUserUseCase = new CreateUserUseCase(userRepo, passwordHashing);
+    const getUserById = new GetUserByIdUseCase(userRepo);
+    const fakeEmailSender = new FakeEmailSender();
+    const createExpenseUseCase = new CreateExpenseUseCase(
+      expenseRepo,
+      getUserById,
+      fakeEmailSender,
     );
-    const editExpenseUseCase = new EditExpenseUseCase(repository);
+    const editExpenseUseCase = new EditExpenseUseCase(expenseRepo);
 
-    const userOrError = User.create(
-      { email: 'diego@gmail.com', name: 'Diego', password: 'Test1234' },
-      'userID',
-    );
-    const user = userOrError.value as User;
+    return { createExpenseUseCase, editExpenseUseCase, createUserUseCase };
+  };
 
-    await userRepo.save(user);
+  it('should edit a expense', async function () {
+    const { createExpenseUseCase, createUserUseCase, editExpenseUseCase } =
+      makeTest();
 
-    const expenseOrError = await createUseCase.execute(
+    const userOrError = await createUserUseCase.execute({
+      email: 'diego@gmail.com',
+      name: 'Diego',
+      password: 'Test1234',
+    });
+    const user = userOrError.value as CreateUserOutput;
+
+    const expenseOrError = await createExpenseUseCase.execute(
       {
         description: 'expense description',
         date: '2022-04-10',
         amount: 120,
       },
-      'userID',
+      user.id,
     );
 
     const expense = expenseOrError.value as ExpenseOutput;
 
     const editedExpenseOrError = await editExpenseUseCase.execute(
       expense.id,
-      'userID',
+      user.id,
       {
         description: 'new expense description',
       },
@@ -52,24 +64,31 @@ describe('EditExpenseUseCase Tests', function () {
   });
 
   it('should return a error if a invalid parameter is passed', async function () {
-    const repository = new ExpenseInMemoryRepository();
-    const createUseCase = new CreateExpenseUseCase(repository);
-    const editExpenseUseCase = new EditExpenseUseCase(repository);
+    const { createExpenseUseCase, editExpenseUseCase, createUserUseCase } =
+      makeTest();
 
-    const expenseOrError = await createUseCase.execute(
+    const userOrError = await createUserUseCase.execute({
+      email: 'diego@gmail.com',
+      name: 'Diego',
+      password: 'Test1234',
+    });
+    const user = userOrError.value as CreateUserOutput;
+
+    const expenseOrError = await createExpenseUseCase.execute(
       {
         description: 'expense description',
         date: '2022-04-10',
         amount: 120,
       },
-      'userID',
+      user.id,
     );
 
     const expense = expenseOrError.value as ExpenseOutput;
+    console.log(expense);
 
     const editedExpenseOrError = await editExpenseUseCase.execute(
       expense.id,
-      'userID',
+      user.id,
       {
         amount: -10,
       },
--- src/@core/usecases/get-all-expense.use-case.spec.ts
+++ src/@core/usecases/get-all-expense.use-case.spec.ts
@@ -1,12 +1,41 @@
+import { User } from '../domain/entities/user/user.entity';
+import { BcryptPasswordHashing } from '../infra/bcrypt/bcrypt-password-hashing';
 import { ExpenseInMemoryRepository } from '../infra/expense-in-memory.repository';
+import { UserInMemoryRepository } from '../infra/user-in-memory.repository';
 import { CreateExpenseUseCase } from './create-expense.use-case';
+import { CreateUserOutput, CreateUserUseCase } from './create-user.use-case';
 import { GetAllExpensesUseCase } from './get-all-expense.use-case';
+import { GetUserByIdUseCase } from './get-user-by-id.use-case';
+import { FakeEmailSender } from './test-utils/fake-email-sender';
 
 describe('GetAllExpensesUseCase Tests', function () {
   it('should return all expenses from a specific user', async function () {
     const repository = new ExpenseInMemoryRepository();
-    const createExpenseUseCase = new CreateExpenseUseCase(repository);
+    const userRepo = new UserInMemoryRepository();
+    const getUserById = new GetUserByIdUseCase(userRepo);
+    const passwordHasher = new BcryptPasswordHashing();
+    const createUser = new CreateUserUseCase(userRepo, passwordHasher);
+    const fakeEmailSender = new FakeEmailSender();
+    const createExpenseUseCase = new CreateExpenseUseCase(
+      repository,
+      getUserById,
+      fakeEmailSender,
+    );
     const getAllExpensesUseCase = new GetAllExpensesUseCase(repository);
+
+    const user1OrError = await createUser.execute({
+      name: 'user1',
+      email: 'user1@gmail.com',
+      password: 'test',
+    });
+    const user2OrError = await createUser.execute({
+      name: 'user2',
+      email: 'user2@gmail.com',
+      password: 'test',
+    });
+
+    const user1 = user1OrError.value as CreateUserOutput;
+    const user2 = user2OrError.value as CreateUserOutput;
 
     await createExpenseUseCase.execute(
       {
@@ -14,7 +43,7 @@ describe('GetAllExpensesUseCase Tests', function () {
         date: '2023-04-10',
         amount: 121,
       },
-      'user1',
+      user1.id,
     );
 
     await createExpenseUseCase.execute(
@@ -23,7 +52,7 @@ describe('GetAllExpensesUseCase Tests', function () {
         date: '2023-04-10',
         amount: 121,
       },
-      'user1',
+      user1.id,
     );
 
     await createExpenseUseCase.execute(
@@ -32,10 +61,10 @@ describe('GetAllExpensesUseCase Tests', function () {
         date: '2023-04-10',
         amount: 121,
       },
-      'user2',
+      user2.id,
     );
 
-    const expenses = await getAllExpensesUseCase.execute('user1');
+    const expenses = await getAllExpensesUseCase.execute(user1.id);
 
     expect(expenses).toHaveLength(2);
     expect(expenses[1].description).toBe('expense description 2');
