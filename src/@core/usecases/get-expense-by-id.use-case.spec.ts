4a79a5b9fd89ea8e337b3780f11b10b2ac3a79bd
dieegomr <dmendes.rocha@gmail.com>
2023-04-18 08:16:03 -0300

    test: better get expense tests

--- src/@core/usecases/get-expense-by-id.use-case.spec.ts
+++ src/@core/usecases/get-expense-by-id.use-case.spec.ts
@@ -1,20 +1,62 @@
+import { User } from '../domain/entities/user/user.entity';
+import {
+  EmailOptions,
+  EmailSender,
+} from '../domain/interfaces/email-sender.interface';
 import { ExpenseInMemoryRepository } from '../infra/expense-in-memory.repository';
+import { UserInMemoryRepository } from '../infra/user-in-memory.repository';
 import { CreateExpenseUseCase } from './create-expense.use-case';
 import { ExpenseNotFoundError } from './errors/get-expense-by-id.error';
 import {
   ExpenseOutput,
   GetExpenseByIdUseCase,
 } from './get-expense-by-id.use-case';
+import { GetUserByIdUseCase } from './get-user-by-id.use-case';
 
 describe('GetExpenseByIdUseCase Tests', function () {
-  const expenseRepo = new ExpenseInMemoryRepository();
-  const createExpense = new CreateExpenseUseCase(expenseRepo);
-  const getExpense = new GetExpenseByIdUseCase(expenseRepo);
+  const makeTest = () => {
+    class FakeEmailSender implements EmailSender {
+      async sendEmail(options: EmailOptions) {
+        console.log(options);
+      }
+    }
 
-  let expense: ExpenseOutput;
+    const fakeEmailSender = new FakeEmailSender();
+    const expenseRepo = new ExpenseInMemoryRepository();
+    const userRepo = new UserInMemoryRepository();
+    const getUserById = new GetUserByIdUseCase(userRepo);
+    const createExpense = new CreateExpenseUseCase(
+      expenseRepo,
+      getUserById,
+      fakeEmailSender,
+    );
+    const getExpense = new GetExpenseByIdUseCase(expenseRepo);
 
-  beforeEach(async function () {
-    const output = await createExpense.execute(
+    return { getExpense, createExpense, userRepo };
+  };
+
+  it('should return an error if could not find an expense because wrong user id', async function () {
+    const { getExpense, createExpense } = makeTest();
+
+    const expenseOrError = await createExpense.execute(
+      {
+        description: 'description1',
+        date: '2023-04-10',
+        amount: 121,
+      },
+      'userId1',
+    );
+    const expense = expenseOrError.value as ExpenseOutput;
+
+    const foundExpense = await getExpense.execute('userId2', expense.id);
+
+    expect(foundExpense.value).toBeInstanceOf(ExpenseNotFoundError);
+  });
+
+  it('should return an error if could not find an expense because wrong expense id', async function () {
+    const { getExpense, createExpense } = makeTest();
+
+    await createExpense.execute(
       {
         description: 'description1',
         date: '2023-04-10',
@@ -23,17 +65,37 @@ describe('GetExpenseByIdUseCase Tests', function () {
       'userId1',
     );
 
-    expense = output.value as ExpenseOutput;
-  });
-
-  it('should return an error if could not find an expense', async function () {
-    const foundExpense = await getExpense.execute('wrongId');
+    const foundExpense = await getExpense.execute('userId1', 'wrongExpenseId');
 
     expect(foundExpense.value).toBeInstanceOf(ExpenseNotFoundError);
   });
 
   it('should return an expense if searched by a existent id', async function () {
-    const result = await getExpense.execute(expense.id);
+    const { getExpense, createExpense, userRepo } = makeTest();
+
+    const userOrErro = User.create(
+      {
+        email: 'diego@gmail.com',
+        name: 'Diego',
+        password: 'Test@1234',
+      },
+      'userId',
+    );
+    const user = userOrErro.value as User;
+
+    await userRepo.save(user);
+
+    const expenseOrError = await createExpense.execute(
+      {
+        description: 'description1',
+        date: '2023-04-10',
+        amount: 121,
+      },
+      'userId',
+    );
+    const expense = expenseOrError.value as ExpenseOutput;
+
+    const result = await getExpense.execute('userId', expense.id);
 
     const foundExpense = result.value as ExpenseOutput;
 
