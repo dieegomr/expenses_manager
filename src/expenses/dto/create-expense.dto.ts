6dbaa6cf6de837a022b553776fe10e12bfab384f
dieegomr <dmendes.rocha@gmail.com>
2023-04-20 10:04:16 -0300

    feat: add validations to create expense dto

--- src/expenses/dto/create-expense.dto.ts
+++ src/expenses/dto/create-expense.dto.ts
@@ -1,6 +1,23 @@
+import {
+  IsString,
+  IsNotEmpty,
+  Length,
+  IsPositive,
+  Validate,
+} from 'class-validator';
+import { BeforeTodayValidator } from './validator/before-today.validator';
+
 export class CreateExpenseDto {
+  @IsString()
+  @IsNotEmpty()
+  @Length(1, 191)
   description: string;
+
+  @IsNotEmpty()
+  @Validate(BeforeTodayValidator)
   date: string;
-  user: string;
+
+  @IsPositive()
+  @IsNotEmpty()
   amount: number;
 }
