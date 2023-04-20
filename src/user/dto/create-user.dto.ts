58cd59091a04382f57f35b39b5c7ab331160b897
dieegomr <dmendes.rocha@gmail.com>
2023-04-20 10:03:45 -0300

    feat: add class validator to create user dto

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,5 +1,15 @@
+import { IsString, IsNotEmpty, IsEmail } from 'class-validator';
+
 export class CreateUserDto {
+  @IsString()
+  @IsNotEmpty()
   name: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @IsEmail()
   email: string;
+
+  @IsNotEmpty()
   password: string;
 }
