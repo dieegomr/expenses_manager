b4984e60331f7357bc74b021b44210b777ca0a46
dieegomr <dmendes.rocha@gmail.com>
2023-04-20 10:07:58 -0300

    feat: add class validator to login dto

--- src/auth/dto/login.dto.ts
+++ src/auth/dto/login.dto.ts
@@ -1,4 +1,10 @@
-export type LoginDto = {
+import { IsNotEmpty, IsEmail } from 'class-validator';
+
+export class LoginDto {
+  @IsEmail()
+  @IsNotEmpty()
   email: string;
+
+  @IsNotEmpty()
   password: string;
-};
+}
